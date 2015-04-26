3d52f85d1837335e7677c59602e1fc326eb5e59c
tugberkugurlu <tugberkugurlu@gmail.com>
2015-04-26 20:21:16 +0100

    added testing infra which is no where need what it should be

---
+++ test/k-http.Tests/Common/TestFileTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Framework.Expiration.Interfaces;
+
+namespace KHttp.Tests.Common
+{
+    internal class TestFileTrigger : IExpirationTrigger
+    {
+        public bool ActiveExpirationCallbacks { get; } = false;
+
+        public bool IsExpired { get; set; }
+
+        public IDisposable RegisterExpirationCallback(Action<object> callback, object state)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
