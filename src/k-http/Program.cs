829e8b69d83449444962092bfa6061d27472298e
tugberkugurlu <tugberkugurlu@gmail.com>
2015-04-29 01:23:43 +0100

    first pass at cmdline app stuff

--- src/k-http/Program.cs
+++ src/k-http/Program.cs
@@ -6,6 +6,9 @@ using Microsoft.Framework.Runtime;
 using Microsoft.Framework.Logging;
 using System.Threading;
 using Microsoft.AspNet.Hosting.Internal;
+using Microsoft.Framework.ConfigurationModel;
+using Microsoft.Framework.Runtime.Common.CommandLine;
+using System.Reflection;
 
 namespace KHttp
 {
@@ -23,6 +26,8 @@ namespace KHttp
 
         public void Main(string[] args)
         {
+            var app = new StaticServerConsoleApplication(args);
+
             var configurationBuilder = new ConfigurationBuilder(args);
             var config = configurationBuilder.Build();
             var webHostBuilder = new WebHostBuilder(_hostServiceProvider, config)
@@ -58,4 +63,88 @@ namespace KHttp
             shutdownHandle.WaitOne();
         }
     }
+
+    public class StaticServerConsoleApplication
+    {
+        private readonly string[] _args;
+        private readonly Configuration _configuration;
+        private readonly StaticServerAppOptions _options;
+        private readonly CommandLineApplication _app;
+
+        public StaticServerConsoleApplication(string[] args)
+        {
+            if(args == null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
+            var options = new StaticServerAppOptions();
+            var app = new CommandLineApplication();
+            app.Name = "k-http";
+            app.FullName = app.Name;
+            app.HelpOption("-?|-h|--help");
+            app.VersionOption("--version", GetVersion);
+            var pathArgument = app.Argument("[path]", "Path to directory to be served through HTTP, default is current directory", c => 
+            {
+                c.Value = Environment.CurrentDirectory;
+            });
+            var portOption = app.Option("-p|--port <PORT>", "TCP port to use, default is 5000", CommandOptionType.SingleValue);
+
+            app.Execute(args);
+
+            ushort port = 5000;
+            if (portOption.HasValue())
+            {
+                if(!ushort.TryParse(portOption.Value(), out port))
+                {
+                    throw new ArgumentException("TODO: Port number is not valid");
+                }
+            }
+
+            options.Path = pathArgument.Value;
+            options.Port = port;
+
+            _app = app;
+            _options = options;
+        }
+
+        public Configuration Configuration
+        {
+            get
+            {
+                return _configuration;
+            }
+        }
+
+        public StaticServerAppOptions Options
+        {
+            get
+            {
+                return _options;
+            }
+        }
+
+        public void OutputHelp()
+        {
+            _app.ShowHelp();
+        }
+
+        private static string GetVersion()
+        {
+            var assembly = typeof(StaticServerConsoleApplication).GetTypeInfo().Assembly;
+            var assemblyInformationalVersionAttribute = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+            return assemblyInformationalVersionAttribute.InformationalVersion;
+        }
+    }
+
+    public class StaticServerAppOptions
+    {
+        public string Path { get; set; }
+        public int Port { get; set; }
+    }
+
+    // Things happening here
+    // 1-) Commandline help/version/info thing
+    // 3-) Cmdline argument validation
+    // 4-) 
 }
